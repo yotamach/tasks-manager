d0430cde9ecd7e65687b90e6866c93ff4f71882e
Yotam Achrak <yotam2585@gmail.com>
2020-10-19 23:32:06 +0300

    configure eslint to server

--- server/index.js
+++ server/index.js
@@ -18,12 +18,12 @@ const mongoose = require("mongoose");
 console.log(config.mongoURI);
 
 const connect = mongoose.connect(`mongodb+srv://${config.userName}:${config.password}@${config.mongoURI}${config.DBname}?retryWrites=true&w=majority`,
-  {
-    useNewUrlParser: true, useUnifiedTopology: true,
-    useCreateIndex: true, useFindAndModify: false
-  })
-  .then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err));
+	{
+		useNewUrlParser: true, useUnifiedTopology: true,
+		useCreateIndex: true, useFindAndModify: false
+	})
+	.then(() => console.log('MongoDB Connected...'))
+	.catch(err => console.log(err));
 
 app.use(cors())
 
@@ -45,18 +45,18 @@ app.use('/uploads', express.static('uploads'));
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
 
-  // Set static folder   
-  // All the javascript and css files will be read and served from this folder
-  app.use(express.static("client/build"));
+	// Set static folder   
+	// All the javascript and css files will be read and served from this folder
+	app.use(express.static("client/build"));
 
-  // index.html for all page routes    html or routing and naviagtion
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  });
+	// index.html for all page routes    html or routing and naviagtion
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+	});
 }
 
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
-  console.log(`Server Listening on ${port}`)
+	console.log(`Server Listening on ${port}`)
 });
