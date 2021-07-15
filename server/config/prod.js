cae79681cbda613097951e2bc6feb451d16f90ac
Yotam Achrak <yotam2585@gmail.com>
2021-07-15 23:15:45 +0300

    configuration

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,7 @@
 module.exports = {
-	mongoURI:process.env.MONGO_URI
+	mongoURI: process.env.MONGO_URI,
+	DBname: process.env.MONGO_DB,
+	userName: process.env.MONGO_USERNAME,
+	password: process.env.MONGO_PASSWORD,
 }
+
